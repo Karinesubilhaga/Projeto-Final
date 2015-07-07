c4b893cfa0da4e2fe124baccd5021c3f0012df72
Karine <karinesubilhaga@yahoo.com.br>
2015-07-07 20:12:27 -0300

    aula07jul

--- Cadastro.php
+++ Cadastro.php
@@ -1,7 +1,10 @@
 <html>
     <head>
         <title>Homepage Consultora de Beleza Mary Kay</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
         <meta charset="ISO-8859-1">
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
         <link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
@@ -10,7 +13,7 @@
         <link href="css/styles.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <div  class = "container" >
+                <div  class = "container" >
             <div class="row">  
                 <div class="col-md-4">
                     <img src="img/logoMK.png" class="logo">
@@ -51,56 +54,68 @@
                 </ul>    
             </nav>        
 
-            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                <!-- Indicators -->
-                <ol class="carousel-indicators">
-                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                </ol>
+ <?php
+// RECEBENDO OS DADOS PREENCHIDOS DO FORMULÁRIO !
+$nome= $_POST ["nome"];//atribuição do campo "nome" vindo do formulário para variavel
+$email= $_POST ["email"];//atribuição do campo "email" vindo do formulário para variavel
+$ddd= $_POST ["ddd"];//atribuição do campo "ddd" vindo do formulário para variavel
+$tel= $_POST ["telefone"];//atribuição do campo "telefone" vindo do formulário para variavel
+$endereco= $_POST ["endereco"];//atribuição do campo "endereco" vindo do formulário para variavel
+$cidade= $_POST ["cidade"];//atribuição do campo "cidade" vindo do formulário para variavel
+$estado= $_POST ["estado"];//atribuição do campo "estado" vindo do formulário para variavel
+$bairro = $_POST ["bairro"];//atribuição do campo "bairro" vindo do formulário para variavel
+$pais= $_POST ["pais"];//atribuição do campo "pais" vindo do formulário para variavel
+$login= $_POST ["login"];//atribuição do campo "login" vindo do formulário para variavel
+$senha= $_POST ["senha"];//atribuição do campo "senha" vindo do formulário para variavel
+$news= $_POST ["news"];//atribuição do campo "news" vindo do formulário para variavel
+$sexo= $_POST ["sexo"];//atribuição do campo "sexo" vindo do formulário para variavel
+ 
+//Gravando no banco de dados ! conectando com o localhost - mysql
+$conexao = mysql_connect("localhost","root"); //localhost é onde esta o banco de dados.
+if (!$conexao)
+die ("Erro de conexão com localhost, o seguinte erro ocorreu -> ".mysql_error());
+ 
+//conectando com a tabela do banco de dados
+$banco = mysql_select_db("clientes",$conexao); //nome da tabela que deseja que seja inserida os dados cadastrais
+if (!$banco)
+die ("Erro de conexão com banco de dados, o seguinte erro ocorreu -> ".mysql_error());
+ 
+ 
+//Query que realiza a inserção dos dados no banco de dados na tabela indicada acima
+$query = "INSERT INTO `clientes` ( `nome` , `email` , `sexo` , `ddd` , `telefone` , `endereço` , `cidade` , `estado` , `bairro` , `país` , `login` , `senha` , `news` , `id` )
+VALUES ('$nome', '$email', '$sexo', '$ddd', '$tel', '$endereco', '$cidade', '$estado', '$bairro', '$pais', '$login', '$senha', '$news', '')";
+mysql_query($query,$conexao);
+########## ? Explicação da query ? ##########
+#$query = nome da variavel que decidi#
+#utilizar para realizar a operação.#
+#############################################
+#clientes = nome da tabela que será salvo#
+#os dados do cadastro do cliente#
+#############################################
+#nome, email, sexo, ddd, telefone,#
+#endereço, cidade, estado, bairro, país,#
+#login, senha, news, id.#
+##
+#São apenas os nomes dos campos que #
+#constam na tabela clientes.#
+#############################
+#VALUES = indica que serão inseridos os#
+#seguintes valores.#
+#############################################
+#$nome, $email, $sexo, $ddd, $telefone,#
+#$endereço, $cidade, $estado, $bairro, #
+#$país, $login, $senha, $news, $id.#
+#############################
+#São apenas as variaveis a qual eu#
+#atribui os valores digitados no formulá-#
+#rio.#
+#############################################
+echo "Seu cadastro foi realizado com sucesso!Agradecemos a atenção.";
+//mensagem que é escrita quando os dados são inseridos normalmente.
+?>
 
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner" role="listbox">
-                    <div class="item active">
-                        <img src="img/make53.png" class="makes" >
-                        <div class="carousel-caption">
-                            ...
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="img/mk.png" class="base" >
-                        <div class="carousel-caption">
-                            ...
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="img/pele53.png" class="pele" >
-                        <div class="carousel-caption">
-                            ...
-                        </div>
-                    </div>
-                    ...
-                </div>
+</html>
 
-                <!-- Controls -->
-                <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-            <div class="row">
-                <div class="col-md-5">
-                    <a href="http://www.marykayintouch.com.br/educacao_de_vendas_2015-v2"> <img src="img/treinamento.PNG" class="treinamento"></a>
-
-                </div>
-                <div class="col-md-5">
-                    <a href="http://marykay.pciims.com.br/consultora.aspx"> <img src="img/comprar.png" class="compras"></a>                   
-                </div>
-            </div>
             <div class="row rodape" >
                 <div class="col-sm-4">
                     <nav id="menuinfdir">
@@ -131,3 +146,4 @@
     </div>
 </body>
 </html>
+
